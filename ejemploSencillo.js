5eaf6625d5e97ebaa640e58195da987eff0ee94d
teppovesterinen <teppo.vesterinen@gmail.com>
2014-11-12 19:03:42 +0200

    lolzers

---
+++ ejemploSencillo.js
@@ -0,0 +1,15 @@
+var http = require('http');
+var miServidor = http.createServer(
+   function (req, res) {
+   res.writeHead(200, { 'Content-Type': 'text/html' });
+   res.end('<marquee>Node y HTTP</marquee>');
+   var d = new Date();
+   var horas = d.getHours();
+   var minutos = d.getMinutes();
+   if (minutos<10) minutos = "0" + minutos;
+   var segundos = d.getSeconds();
+   if (segundos<10) segundos = "0" + segundos;
+   var ahora = horas + ":" + minutos + ":" + segundos;
+   console.log('Alguien ha accedido a las ' + ahora);
+   });
+miServidor.listen(8000);
