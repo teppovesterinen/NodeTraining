f047ba8c55badc4931ee6d0109cb44072e21cf93
teppovesterinen <teppo.vesterinen@gmail.com>
2014-11-13 12:48:54 +0200

    changezzzzz

--- eventSimple.js
+++ eventSimple.js
@@ -1,8 +1,29 @@
-var http = require('http');
+var ev = require('events');
+var emitter = new ev.EventEmitter;
+// Names of the events.
+var e1 = "print";
+var e2 = "read";
+// Auxiliary variables.
+var num1 = 0;
+var num2 = 0;
 
-var server = http.createServer(function(req, res) {
-   res.writeHead(200);
-   var x = '-not much, mah nigga';
-   res.end('wha d\'up dawg' + x);
-});
-server.listen(8080);
+// Listener functions are registered on 
+// the event emitter.
+emitter.on(e1, function() {
+   console.log( "Event " + e1 + " has " + 
+      "happened " + ++num1 + " times.")});
+emitter.on(e2, function() {
+   console.log( "Event " + e2 + " has " +
+      "happened " + ++num2 + " times.")});
+// There might be more than one listener 
+// for the same event.
+emitter.on(e1, function() {console.log(
+   "Something has been printed!!");});
+
+// Generate the events periodically...
+// First event generated every 2 seconds.
+setInterval( function() {
+   emitter.emit(e1);}, 2000 );
+// Second event generated every 3 seconds.
+setInterval( function() {
+   emitter.emit(e2);}, 8000 );
--- single.js
+++ single.js
@@ -1,1 +1,27 @@
-console.log('Whats up fucker!');
+function fibonacci(n) {
+return (n<2) ? 1 : fibonacci(n-2) + fibonacci(n-1);
+}
+
+console.log("Iniciando ejecución...");
+
+// Esperar 10 ms para escribir un mensaje.
+setTimeout( function() {
+console.log( "M1: Quiero escribir esto..." );
+}, 10 );
+
+// A esta función le cuesta más de 1 segundo
+// ejecutarse...
+var j = fibonacci(40);
+
+function otroMensaje(m,u) {
+console.log( m + ": El resultado es: " + u );
+}
+
+// Pero M2 se escribe antes que M1 porque el 
+// hilo "principal" rara vez se suspende...
+otroMensaje("M2",j);
+
+// M3 ya se escribe tras M1. 
+setTimeout( function() {
+otroMensaje("M3",j);
+}, 1 );
