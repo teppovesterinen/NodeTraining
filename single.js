00002e02eb846f9389eb093ce767bdfd5c62c273
teppovesterinen <teppo.vesterinen@gmail.com>
2014-11-13 12:53:31 +0200

    moar-changezzzzzz

--- single.js
+++ single.js
@@ -1,12 +1,12 @@
 function fibonacci(n) {
-return (n<2) ? 1 : fibonacci(n-2) + fibonacci(n-1);
+   return (n<2) ? 1 : fibonacci(n-2) + fibonacci(n-1);
 }
 
 console.log("Iniciando ejecución...");
 
 // Esperar 10 ms para escribir un mensaje.
 setTimeout( function() {
-console.log( "M1: Quiero escribir esto..." );
+   console.log( "M1: Quiero escribir esto..." );
 }, 10 );
 
 // A esta función le cuesta más de 1 segundo
@@ -14,7 +14,7 @@ console.log( "M1: Quiero escribir esto..." );
 var j = fibonacci(40);
 
 function otroMensaje(m,u) {
-console.log( m + ": El resultado es: " + u );
+   console.log( m + ": El resultado es: " + u );
 }
 
 // Pero M2 se escribe antes que M1 porque el 
@@ -23,5 +23,5 @@ otroMensaje("M2",j);
 
 // M3 ya se escribe tras M1. 
 setTimeout( function() {
-otroMensaje("M3",j);
+   otroMensaje("M3",j);
 }, 1 );
